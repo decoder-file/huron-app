875b12822a9b3e0ccbae5b594c6d9846d6ccdf5f
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-09-26 19:18:47 -0300

    fix: adjusting button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,17 @@
 import { TouchableOpacityProps } from "react-native";
 
-interface ButtonProps extends TouchableOpacityProps {
+export interface ButtonProps extends TouchableOpacityProps {
   title: string;
+  type?: "primary" | "secondary";
+  icon?: React.ReactNode;
 }
 
 import { Container, Title } from "./styles";
 
-export function Button({ title, ...rest }: ButtonProps) {
+export function Button({ title, icon, ...rest }: ButtonProps) {
   return (
     <Container activeOpacity={0.8} {...rest}>
+      {icon && icon}
       <Title>{title}</Title>
     </Container>
   );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,15 +1,19 @@
 import styled from "styled-components/native";
 
+import { ButtonProps } from "./index";
+
 export const Container = styled.TouchableOpacity`
   width: 100%;
   align-items: center;
   justify-content: center;
   padding: 18px;
   border-radius: 18px;
+  flex-direction: row;
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<ButtonProps>`
   font-family: ${({ theme }) => theme.FONT_FAMILY.MEDIUM};
   font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  color: ${({ theme }) => theme.COLORS.GREEN_200};
+  color: ${({ type }) => (type === "primary" ? "#48E1B" : "#FFFFFF")};
+  margin-left: 10px;
 `;
